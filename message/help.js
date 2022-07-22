e4849d8eeba74763a7b5f6ded8ac89258d0327d2
jayaaluminium <amotor700@gmail.com>
2022-07-22 18:46:51 +0800

    Upload di Github

---
+++ message/help.js
@@ -0,0 +1,12 @@
+exports.txtSewabot = (prefix) => {
+return `
+ Permanent : Rp5.000
+`
+}
+exports.txtDonate = (prefix) => {
+return `
+▷_*DONASI BOT*_
+ _- DANA : 083114072346_
+ _- QRIS : https://linktr.ee/KadekBotz
+`
+}
